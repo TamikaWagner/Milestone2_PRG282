7548a1f9fd04c23fce79ce43bab4bca2bb3bd133
Tamika Wagner <91661526+TamikaWagner@users.noreply.github.com>
2021-10-06 11:25:44 +0200

    Add project files.

---
+++ DataLayer/SQLDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_Milestone2.DataLayer
+{
+    class SQLDB
+    {
+    }
+}
---
+++ Student-Sql/SQLQuery1.sql
@@ -0,0 +1,11 @@
+create table DetailsStudent
+(
+  Student_Number int,
+  Student_Name varchar(255) not null,
+  Gender varchar(255) not null,
+  Date_Of_Birth date,
+  Phone varchar(14) not null unique,
+  Student_Address varchar(255) not null,
+  Module_Codes varchar not null,
+  Student_Image VARBINARY(MAX)
+  )
