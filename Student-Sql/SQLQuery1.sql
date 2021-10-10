94a5f524b81dc184f635a9884f1659c29e889db2
ChristianZac <62380071+ChristianZac@users.noreply.github.com>
2021-10-10 18:51:01 +0200

    Phone amount update

--- Student-Sql/SQLQuery1.sql
+++ Student-Sql/SQLQuery1.sql
@@ -4,7 +4,7 @@ create table DetailsStudent
   Student_Name varchar(255) not null,
   Gender varchar(255) not null,
   Date_Of_Birth date,
-  Phone varchar(14) not null unique,
+  Phone varchar(10) not null unique,
   Student_Address varchar(255) not null,
   Module_Codes varchar not null,
   Student_Image VARBINARY(MAX)
