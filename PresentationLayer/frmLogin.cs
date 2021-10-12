615d7bec1ff980018a4207d429840c0e162265f0
ChristianZac <62380071+ChristianZac@users.noreply.github.com>
2021-10-12 15:50:22 +0200

    Try 1

--- PresentationLayer/frmLogin.cs
+++ PresentationLayer/frmLogin.cs
@@ -40,7 +40,8 @@ namespace PRG282_Milestone2.PresentationLayer
         private void BtnLogin_Click(object sender, EventArgs e)
         {
             cl.checkLogin(txtusername.Text, txtpassword.Text , usersInSystem);
-          
+            frmMenu fm = new frmMenu();
+            fm.Show();
         }
 
         private void brnRegister_Click(object sender, EventArgs e)
